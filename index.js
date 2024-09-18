d2e0298b0fe4e65edd62fefc0dd44c0bf15bfce5
Qianni Zhang <zhangqianni24@gmail.com>
2024-09-18 14:02:08 +0800

    script user input part added.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // TODO: this file! :)
+
+// === State ===
+const state = {
+    bank:[],
+    odd:[],
+    even:[],
+};
+
+ function addNumber(number){
+    bank.push(number);
+ }
+
+// === Script ===
+
+render();
+
+/** Adds a number to the bank according to user prompt*/
+function addNumber(){
+    const $form = document.querySelector("form")
+    $form.addEventListener("submit", (event)=>{
+        event.preventDefault();
+
+        const $numberInput = document.querySelector("#number");
+        addNumber($numberInput.value)
+    })
+}
+
+render();
